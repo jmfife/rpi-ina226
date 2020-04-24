052a6d7856ab32c1d1966da60602099b8a73eedd
Mike Fife <jmfife@gmail.com>
2020-04-24 14:52:00 -0700

    try to add --emulate option

--- ina226.c
+++ ina226.c
@@ -5,6 +5,8 @@
 #include <math.h>
 #include <unistd.h>
 #include <wiringPiI2C.h>
+#include <argp.h>
+#include <stdbool.h>
 #include "ina226.h"
 
 #define INA226_ADDRESS 0x40
@@ -13,20 +15,44 @@ int fd;
 uint64_t config;
 float current_lsb;
 
+const char *argp_program_version = "ina226 1.0";
+const char *argp_program_bug_address = "https://github.com/jmfife/rpi-ina226";
+static char doc[] = "Use INA226 chip in conjunction with a Raspberry Pi to measure DC voltage and current";
+static char args_doc[] = "";
+static struct argp_option options[] = {
+	{ "emulate", 'e', 0, 0, "Run in emulation mode"},
+	{ 0 }
+};
+
+struct arguments {
+	bool emulate;
+};
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state) {
+	struct arguments *arguments = state->input;
+	switch (key) {
+		case 'e': arguments->emulate = true; break;
+		case ARGP_KEY_ARG: return 0;
+		default: return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+static struct argp argp = { options, parse_opt, args_doc, doc, 0, 0, 0 };
+
 uint16_t read16(int fd, uint8_t ad){
 	uint16_t result = wiringPiI2CReadReg16(fd,ad);
 	// Chip uses different endian
 	return  (result<<8) | (result>>8);
 }
 
-void write16(int fd, uint8_t ad, uint16_t value){
+void write16(int fd, uint8_t ad, uint16_t value) {
 	// Chip uses different endian
 	wiringPiI2CWriteReg16(fd,ad,(value<<8)|(value>>8));
 }
 
 // R of shunt resistor in ohm. Max current in Amp
-void ina226_calibrate(float r_shunt, float max_current)
-{
+void ina226_calibrate(float r_shunt, float max_current) {
 	current_lsb = max_current / (1 << 15);
 	float calib = 0.00512 / (current_lsb * r_shunt);
 	uint16_t calib_reg = (uint16_t) floorf(calib);
@@ -39,18 +65,16 @@ void ina226_calibrate(float r_shunt, float max_current)
 	write16(fd,INA226_REG_CALIBRATION, calib_reg);
 }
 
-void ina226_configure(uint8_t bus, uint8_t shunt, uint8_t average, uint8_t mode)
-{
+void ina226_configure(uint8_t bus, uint8_t shunt, uint8_t average, uint8_t mode) {
 	config = (average<<9) | (bus<<6) | (shunt<<3) | mode;
 	write16(fd,INA226_REG_CONFIGURATION, config);
 }
 
-uint16_t ina226_conversion_ready()
-{
+uint16_t ina226_conversion_ready() {
 	return read16(fd,INA226_REG_MASK_ENABLE) & INA226_MASK_ENABLE_CVRF;
 }
 
-void ina226_wait(){
+void ina226_wait() {
 	uint8_t average = (config>>9) & 7;
 	uint8_t bus = (config>>6) & 7;
 	uint8_t shunt = (config>>3) & 7;
@@ -66,8 +90,7 @@ void ina226_wait(){
 	//printf("%d\n",count);
 }
 
-void ina226_read(float *voltage, float *current, float *power, float* shunt_voltage)
-{
+void ina226_read(float *voltage, float *current, float *power, float* shunt_voltage) {
 	if (voltage) {
 		uint16_t voltage_reg = read16(fd,INA226_REG_BUS_VOLTAGE);
 		*voltage = (float) voltage_reg * 1.25e-3;
@@ -89,17 +112,21 @@ void ina226_read(float *voltage, float *current, float *power, float* shunt_volt
 	}
 }
 
-inline void ina226_reset()
-{
+inline void ina226_reset() {
 	write16(fd, INA226_REG_CONFIGURATION, config = INA226_RESET);
 }
 
-inline void ina226_disable()
-{
+inline void ina226_disable() {
 	write16(fd, INA226_REG_CONFIGURATION, config = INA226_MODE_OFF);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+	struct arguments arguments;
+
+	arguments.emulate = false;
+
+	argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
 	// const float kwh_price = 0.13;
 	float voltage, current, power, shunt;
 	// float energy, price;
@@ -107,31 +134,36 @@ int main() {
 	// char buffer[80];
 	// int trig=1;
 
-	fd = wiringPiI2CSetup(INA226_ADDRESS);
-	if(fd < 0) {
-		printf("Device not found");
-		return -1;
+	if !arguments.emulate {
+		fd = wiringPiI2CSetup(INA226_ADDRESS);
+		}
+		if(fd < 0) {
+			printf("Device not found");
+			return -1;
+		}
+
+		//printf("Manufacturer 0x%X Chip 0x%X\n",read16(fd,INA226_REG_MANUFACTURER),read16(fd,INA226_REG_DIE_ID));
+
+		// Shunt resistor (Ohm), Max Current (Amp)
+		ina226_calibrate(0.009055, 100.0);
+
+		// BUS / SHUNT / Averages / Mode
+		ina226_configure(INA226_TIME_8MS, INA226_TIME_8MS, INA226_AVERAGES_16, INA226_MODE_SHUNT_BUS_CONTINUOUS);
 	}
-
-	//printf("Manufacturer 0x%X Chip 0x%X\n",read16(fd,INA226_REG_MANUFACTURER),read16(fd,INA226_REG_DIE_ID));
-
-	// Shunt resistor (Ohm), Max Current (Amp)
-	ina226_calibrate(0.009055, 20.0);
 
 	// Header
 	//printf("Time, timestamp, bus voltage(V), current (mA), power (mW), shunt voltage (mV), annual energy (kWh), cost ($)\n");
-
-	// BUS / SHUNT / Averages / Mode
-	ina226_configure(INA226_TIME_8MS, INA226_TIME_8MS, INA226_AVERAGES_16, INA226_MODE_SHUNT_BUS_CONTINUOUS);
 
 	for(;;){
 		//ina226_configure(INA226_TIME_8MS, INA226_TIME_8MS, INA226_AVERAGES_16, INA226_MODE_SHUNT_BUS_TRIGGERED);
 		//ina226_wait();
 
-		// Read
-		ina226_read(&voltage, &current, &power, &shunt);
-		// energy = voltage*current*24*365.25/1000000;
-		// price = energy * kwh_price;
+		if !arguments.emulate {
+			// Read
+			ina226_read(&voltage, &current, &power, &shunt);
+			// energy = voltage*current*24*365.25/1000000;
+			// price = energy * kwh_price;
+		 }
 
 		// Timestamp / Date
 		time(&rawtime);
