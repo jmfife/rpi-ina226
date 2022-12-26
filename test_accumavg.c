ad287d32d8484b8a6617bb572d458c5465cf9ebb
Mike Fife <jmfife@gmail.com>
2022-12-26 10:12:38 -0800

    test destroy function

--- test_accumavg.c
+++ test_accumavg.c
@@ -8,5 +8,6 @@ int main()
   AccumAvg_reset2(f, 10.0);
   AccumAvg_accum(f, 20.0, 55.0);
   AccumAvg_accum(f, 35.0, 85.0);
-  printf("Avg: %g should be equal to %g\n", AccumAvg_avg(f), (55.0*10.0 + 85.0*15.0)/25.0);
+  printf("Avg: %g should be equal to %g\n", AccumAvg_avg(f), (55.0 * 10.0 + 85.0 * 15.0) / 25.0);
+  AccumAvg_destroy(f);
 }
